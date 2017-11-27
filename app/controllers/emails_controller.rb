75400a9467b303d490adcabffb3fdb002e8a729f
Huanhuan Huang <hhuang@atlassian.com>
2017-11-27 21:44:15 +1000

    migrate to rspec

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,5 +1,4 @@
 class EmailsController < ApplicationController
   def create
-    
   end
 end
--- test/controllers/emails_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class EmailsControllerTest < ActionDispatch::IntegrationTest
-  test "should reponse successfully" do
-    post emails_url
-    assert_response :success
-  end
-end
